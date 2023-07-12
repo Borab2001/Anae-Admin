3c6069e7dad0cd4b95211a30c8b85e6a096045ae
Bora <borabalos9@gmail.com>
2023-07-12 18:41:43 +1000

    Modal component started + imported Dialog shadcn component

--- anae-admin/app/(root)/page.tsx
+++ anae-admin/app/(root)/page.tsx
@@ -1,9 +1,13 @@
-import { UserButton } from "@clerk/nextjs";
+"use client";
+
+import { Modal } from "@/components/ui/modal";
 
 const SetupPage = () => {
   return (
     <div className="p-4">
-      <UserButton afterSignOutUrl="/" />
+      <Modal title="Test" description="Test description" isOpen onClose={() => {}}>
+        Children
+      </Modal>
     </div>
   );
 }
