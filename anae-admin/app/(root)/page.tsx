ac394eb66851a0f172106884323fb283771ebc37
Bora <borabalos9@gmail.com>
2023-07-13 05:47:14 +1000

    Triggering the modal

--- anae-admin/app/(root)/page.tsx
+++ anae-admin/app/(root)/page.tsx
@@ -1,13 +1,22 @@
 "use client";
 
-import { Modal } from "@/components/ui/modal";
+import { useEffect } from "react";
+
+import { useStoreModal } from "@/hooks/use-store-modal";
 
 const SetupPage = () => {
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
   return (
     <div className="p-4">
-      <Modal title="Test" description="Test description" isOpen onClose={() => {}}>
-        Children
-      </Modal>
+      Root Page
     </div>
   );
 }
