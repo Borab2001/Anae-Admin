96c9ba9747205d9c5881aed11082d40b9e4e0ac9
Bora <borabalos9@gmail.com>
2023-07-20 02:18:57 +1000

    Started the Navbar

--- anae-admin/app/(dashboard)/[storeId]/layout.tsx
+++ anae-admin/app/(dashboard)/[storeId]/layout.tsx
@@ -2,6 +2,7 @@ import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
 import prismadb from "@/lib/prismadb";
+import Navbar from "@/components/navbar";
 
 export default async function DashboardLayout({
     children,
@@ -29,9 +30,7 @@ export default async function DashboardLayout({
 
     return (
         <>
-            <div>
-                Navbar
-            </div>
+            <Navbar />
             {children}
         </>
     )
