c62ab05896409f6fa961236ef6e2c4d24ab912f0
Bora <borabalos9@gmail.com>
2023-07-26 15:14:09 +1000

    Beginning of SettingsForm

--- anae-admin/app/(dashboard)/[storeId]/(routes)/settings/page.tsx
+++ anae-admin/app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -3,6 +3,8 @@ import { redirect } from "next/navigation";
 
 import prismadb from "@/lib/prismadb";
 
+import { SettingsForm } from "./components/settings-form";
+
 interface SettingsPageProps {
     params: {
         storeId: string;
@@ -28,9 +30,12 @@ const SettingsPage: React.FC<SettingsPageProps> = async ({
     if (!store) {
         redirect("/");
     }
+    
     return (
-        <div>
-            Settings
+        <div className="flex-col">
+            <div className="flex-1 space-y-4 p-8 pt-6">
+                <SettingsForm initialData={store} />
+            </div>
         </div>
     );
 }
