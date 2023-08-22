0b3607ba2e1f7e4b30c58b620f28a25bb9004947
Bora <borabalos9@gmail.com>
2023-08-22 20:34:36 +1000

    ApiAlert in settings

---
+++ anae-admin/components/ui/api-alert.tsx
@@ -0,0 +1,31 @@
+import { Server } from "lucide-react";
+
+import { Alert } from "@/components/ui/alert";
+
+interface ApiAlertProps {
+    title: string;
+    description: string;
+    variant: "public" | "admin";
+}
+
+const textMap: Record<ApiAlertProps["variant"], string> = {
+    public: "Public",
+    admin: "Admin"
+}
+
+const variantMap: Record<ApiAlertProps["variant"], string> = {
+    public: "secondary",
+    admin: "destructive"
+};
+
+export const ApiAlert: React.FC<ApiAlertProps> = ({
+    title,
+    description,
+    variant = "public",
+}) => {
+    return (
+        <Alert>
+            <Server className="h-4 w-4" />
+        </Alert>
+    )
+}
