f41266caa6c7365471ca19cd1bd6ed1ee099397d
Bora <borabalos9@gmail.com>
2023-09-25 23:47:20 +1000

    ApiAlert title and description

--- anae-admin/components/ui/api-alert.tsx
+++ anae-admin/components/ui/api-alert.tsx
@@ -1,6 +1,8 @@
-import { Server } from "lucide-react";
+import { Copy, Server } from "lucide-react";
 
-import { Alert } from "@/components/ui/alert";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Badge, BadgeProps } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
 
 interface ApiAlertProps {
     title: string;
@@ -13,7 +15,7 @@ const textMap: Record<ApiAlertProps["variant"], string> = {
     admin: "Admin"
 }
 
-const variantMap: Record<ApiAlertProps["variant"], string> = {
+const variantMap: Record<ApiAlertProps["variant"], BadgeProps["variant"]> = {
     public: "secondary",
     admin: "destructive"
 };
@@ -26,6 +28,20 @@ export const ApiAlert: React.FC<ApiAlertProps> = ({
     return (
         <Alert>
             <Server className="h-4 w-4" />
+            <AlertTitle className="flex items-center gap-x-2">
+                {title}
+                <Badge variant={variantMap[variant]}>
+                    {textMap[variant]}
+                </Badge>
+            </AlertTitle>
+            <AlertDescription className="mt-4 flex items-center justify-between">
+                <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
+                    {description}
+                </code>
+                <Button variant="outline" size="icon" onClick={() => {}} >
+                    <Copy className="h-4 w-4" />
+                </Button>
+            </AlertDescription>
         </Alert>
     )
 }
