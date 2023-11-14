20ea2e484fa22cc9c9b5ddbcd729dc69d33f56e7
Bora <borabalos9@gmail.com>
2023-11-14 12:28:06 +1100

    Blank new billboard page

---
+++ anae-admin/app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -0,0 +1,19 @@
+import prismadb from "@/lib/prismadb";
+
+const BillboardPage = async ({
+    params
+}: {
+    params: { billboardId: string }
+}) => {
+    const billboard = await prismadb.billboard.findUnique({
+        where: {
+            id: params.billboardId
+        }
+    });
+
+    return (
+        <div>Existing Billboard: {billboard?.label}</div>
+    );
+}
+ 
+export default BillboardPage;
