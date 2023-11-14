e30543e00ead49be8acdb7a04ceb3264f586a5da
Bora <borabalos9@gmail.com>
2023-11-14 12:22:30 +1100

    Billboard route on client component

--- anae-admin/app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ anae-admin/app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -1,12 +1,16 @@
 "use client";
 
 import { Plus } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
 
 import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 
 export const BillboardClient = () => {
+    const router = useRouter();
+    const params = useParams();
+            
     return (
         <>
             <div className="flex items-center justify-between">
@@ -14,7 +18,7 @@ export const BillboardClient = () => {
                     title="Billboards (0)"
                     description="Manage billboards for your store"
                 />
-                <Button>
+                <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
                     <Plus className="mr-2 h-4 w-4" />
                     Add New
                 </Button>
