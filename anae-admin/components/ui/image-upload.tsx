b773a6074ff5998517db37e2c30d5001f94332a5
Bora <borabalos9@gmail.com>
2023-11-14 14:53:09 +1100

    Base image-upload component

---
+++ anae-admin/components/ui/image-upload.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+interface ImageUploadProps {
+    disabled?: boolean;
+    onChange: (value: string) => void;
+    onRemove: (value: string) => void;
+    value: string[];
+}
+
+const ImageUpload: React.FC<ImageUploadProps> = ({
+    onChange,
+    onRemove,
+    value
+}) => {
+    const [isMounted, setIsMounted] = useState(false);
+    
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    if (!isMounted) {
+        return null;
+    }
+
+    return (
+        <div>
+            Image Upload
+        </div>
+    )
+}
+
+export default ImageUpload;
