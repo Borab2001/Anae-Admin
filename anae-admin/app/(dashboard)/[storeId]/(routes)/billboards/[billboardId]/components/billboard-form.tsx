b5525132aacb52be350b3658b9a090c840599191
Bora <borabalos9@gmail.com>
2023-11-16 02:20:53 +1100

    Modify the routes in the billboard form

--- anae-admin/app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
+++ anae-admin/app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
@@ -57,9 +57,13 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({
     const onSubmit = async (data: BillboardFormValues) => {
         try {
             setLoading(true);
-            await axios.patch(`/api/stores/${params.storeId}`, data);
+            if (initialData) {
+                await axios.patch(`/api/${params.storeId}/billboards/${params.billboardId}`, data);
+            } else {
+                await axios.post(`/api/${params.storeId}/billboards`, data);
+            }
             router.refresh();
-            toast.success("Great! Store is updated");
+            toast.success(toastMessage);
         } catch (error) {
             toast.error("Oops...Something went wrong");
         } finally {
@@ -70,12 +74,12 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({
     const onDelete = async () => {
         try {
             setLoading(true);
-            await axios.delete(`/api/stores/${params.storeId}`);
+            await axios.delete(`/api/${params.storeId}/billboards/${params.billboardId}`);
             router.refresh();
             router.push("/");
-            toast.success("Store deleted!");
+            toast.success("Billboard deleted!");
         } catch (error) {
-            toast.error("Make sure you removed all products & categories first");
+            toast.error("Make sure you removed all categories using this billboard");
         } finally {
             setLoading(false);
             setOpen(false);
