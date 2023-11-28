898212ee51e36ba1c2bec229948afb0d61b14ef7
Bora <borabalos9@gmail.com>
2023-11-28 14:48:25 +1100

    Cell actions for categories

--- anae-admin/app/(dashboard)/[storeId]/(routes)/categories/components/cell-action.tsx
+++ anae-admin/app/(dashboard)/[storeId]/(routes)/categories/components/cell-action.tsx
@@ -17,10 +17,10 @@ import {
 import { Button } from "@/components/ui/button";
 import { AlertModal } from "@/components/modals/alert-modal";
 
-import { BillboardColumn } from "./columns";
+import { CategoryColumn } from "./columns";
 
 interface CellActionProps {
-    data: BillboardColumn;
+    data: CategoryColumn;
 }
 
 export const CellAction: React.FC<CellActionProps> = ({
@@ -34,17 +34,17 @@ export const CellAction: React.FC<CellActionProps> = ({
 
     const onCopy= (id: string) => {
         navigator.clipboard.writeText(id);
-        toast.success("Billboard ID copied to the clipboard!");
+        toast.success("Category ID copied to the clipboard!");
     };
 
     const onDelete = async () => {
         try {
             setLoading(true);
-            await axios.delete(`/api/${params.storeId}/billboards/${data.id}`);
+            await axios.delete(`/api/${params.storeId}/categories/${data.id}`);
             router.refresh();
-            toast.success("Billboard deleted!");
+            toast.success("Category deleted!");
         } catch (error) {
-            toast.error("Make sure you removed all categories using this billboard");
+            toast.error("Make sure you removed all products using this category first");
         } finally {
             setLoading(false);
             setOpen(false);
@@ -74,7 +74,7 @@ export const CellAction: React.FC<CellActionProps> = ({
                         <Copy className="mr-2 h-4 w-4" />
                         Copy ID
                     </DropdownMenuItem>
-                    <DropdownMenuItem onClick={() => router.push(`/${params.storeId}/billboards/${data.id}`)}>
+                    <DropdownMenuItem onClick={() => router.push(`/${params.storeId}/categories/${data.id}`)}>
                         <Edit className="mr-2 h-4 w-4" />
                         Update
                     </DropdownMenuItem>
