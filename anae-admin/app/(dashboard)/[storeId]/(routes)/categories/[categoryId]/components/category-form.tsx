3751bfddae61a99187448035ddc39ead6af7a8e1
Bora <borabalos9@gmail.com>
2023-11-28 13:58:54 +1100

    Category form routes adapted

--- anae-admin/app/(dashboard)/[storeId]/(routes)/categories/[categoryId]/components/category-form.tsx
+++ anae-admin/app/(dashboard)/[storeId]/(routes)/categories/[categoryId]/components/category-form.tsx
@@ -57,12 +57,12 @@ export const CategoryForm: React.FC<CategoryFormProps> = ({
         try {
             setLoading(true);
             if (initialData) {
-                await axios.patch(`/api/${params.storeId}/billboards/${params.billboardId}`, data);
+                await axios.patch(`/api/${params.storeId}/categories/${params.categoryId}`, data);
             } else {
-                await axios.post(`/api/${params.storeId}/billboards`, data);
+                await axios.post(`/api/${params.storeId}/categories`, data);
             }
             router.refresh();
-            router.push(`/${params.storeId}/billboards`);
+            router.push(`/${params.storeId}/categories`);
             toast.success(toastMessage);
         } catch (error) {
             toast.error("Oops...Something went wrong");
@@ -74,12 +74,12 @@ export const CategoryForm: React.FC<CategoryFormProps> = ({
     const onDelete = async () => {
         try {
             setLoading(true);
-            await axios.delete(`/api/${params.storeId}/billboards/${params.billboardId}`);
+            await axios.delete(`/api/${params.storeId}/categories/${params.categoryId}`);
             router.refresh();
-            router.push(`/${params.storeId}/billboards`);
-            toast.success("Billboard deleted!");
+            router.push(`/${params.storeId}/categories`);
+            toast.success("Category deleted!");
         } catch (error) {
-            toast.error("Make sure you removed all categories using this billboard");
+            toast.error("Make sure you removed all products using this category first");
         } finally {
             setLoading(false);
             setOpen(false);
