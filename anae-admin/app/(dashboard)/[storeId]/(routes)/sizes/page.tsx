3ed271930dd7df0fbcdd081c6bfaf982ad2929ca
Bora <borabalos9@gmail.com>
2023-11-30 18:40:42 +1100

    Size client adapted

--- anae-admin/app/(dashboard)/[storeId]/(routes)/sizes/page.tsx
+++ anae-admin/app/(dashboard)/[storeId]/(routes)/sizes/page.tsx
@@ -2,8 +2,8 @@ import { format } from "date-fns";
 
 import prismadb from "@/lib/prismadb";
 
-import { BillboardClient } from "./components/client";
-import { BillboardColumn } from "./components/columns";
+import { SizeClient } from "./components/client";
+import { SizeColumn } from "./components/columns";
 
 const SizesPage = async ({
     params
@@ -19,20 +19,20 @@ const SizesPage = async ({
         }
     });
 
-    const formattedSizes: BillboardColumn[] = sizes.map((item) => ({
+    const formattedSizes: SizeColumn[] = sizes.map((item) => ({
         id: item.id,
         name: item.name,
-        value: item.value
+        value: item.value,
         createdAt: format(item.createdAt, 'MMMM do, yyyy'),
     }));
 
     return (
         <div className="flex-col">
             <div className="flex-1 space-y-4 p-8 pt-6">
-                <BillboardClient data={formattedSizes} />
+                <SizeClient data={formattedSizes} />
             </div>
         </div>
     );
 }
  
-export default BillboardsPage;
+export default SizesPage;
