639b4933268c72cef61868be0caa9247e4bcd3a6
Bora <borabalos9@gmail.com>
2023-12-01 15:56:15 +1100

    [colorId] page adapted

--- anae-admin/app/(dashboard)/[storeId]/(routes)/colors/[colorId]/page.tsx
+++ anae-admin/app/(dashboard)/[storeId]/(routes)/colors/[colorId]/page.tsx
@@ -2,25 +2,25 @@ import prismadb from "@/lib/prismadb";
 
 import { SizeForm } from "./components/size-form";
 
-const SizePage = async ({
+const ColorPage = async ({
     params
 }: {
-    params: { sizeId: string }
+    params: { colorId: string }
 }) => {
-    const size = await prismadb.size.findUnique({
+    const color = await prismadb.color.findUnique({
         where: {
-            id: params.sizeId
+            id: params.colorId
         }
     });
 
     return (
         <div className="flex-col">
             <div className="flex-1 space-y-4 p-8 pt-6">
-                <SizeForm initialData={size} />
+                <SizeForm initialData={color} />
             </div>
         </div>
         
     );
 }
  
-export default SizePage;
+export default ColorPage;
