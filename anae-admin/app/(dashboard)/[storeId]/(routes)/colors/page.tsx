3e09f57f3bcd0e4e863945381a1bb8affe675913
Bora <borabalos9@gmail.com>
2023-12-01 15:41:31 +1100

    Colors page fixing

---
+++ anae-admin/app/(dashboard)/[storeId]/(routes)/colors/page.tsx
@@ -0,0 +1,38 @@
+import { format } from "date-fns";
+
+import prismadb from "@/lib/prismadb";
+
+import { SizeClient } from "./components/client";
+import { SizeColumn } from "./components/columns";
+
+const ColorsPage = async ({
+    params
+}: {
+    params: { storeId: string }
+}) => {
+    const colors = await prismadb.color.findMany({
+        where: {
+            storeId: params.storeId
+        },
+        orderBy: {
+            createdAt: 'desc'
+        }
+    });
+
+    const formattedColors: SizeColumn[] = colors.map((item) => ({
+        id: item.id,
+        name: item.name,
+        value: item.value,
+        createdAt: format(item.createdAt, 'MMMM do, yyyy'),
+    }));
+
+    return (
+        <div className="flex-col">
+            <div className="flex-1 space-y-4 p-8 pt-6">
+                <SizeClient data={formattedColors} />
+            </div>
+        </div>
+    );
+}
+ 
+export default ColorsPage;
