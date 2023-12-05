ed796452f2c0776f9e9606d4edbe7d4688529848
Bora <borabalos9@gmail.com>
2023-12-05 20:03:17 +1100

    [productId] page adapted

--- anae-admin/app/(dashboard)/[storeId]/(routes)/products/[productId]/page.tsx
+++ anae-admin/app/(dashboard)/[storeId]/(routes)/products/[productId]/page.tsx
@@ -2,25 +2,28 @@ import prismadb from "@/lib/prismadb";
 
 import { BillboardForm } from "./components/billboard-form";
 
-const BillboardPage = async ({
+const ProductPage = async ({
     params
 }: {
-    params: { billboardId: string }
+    params: { productId: string }
 }) => {
-    const billboard = await prismadb.billboard.findUnique({
+    const product = await prismadb.product.findUnique({
         where: {
-            id: params.billboardId
+            id: params.productId
+        },
+        include: {
+            images: true
         }
     });
 
     return (
         <div className="flex-col">
             <div className="flex-1 space-y-4 p-8 pt-6">
-                <BillboardForm initialData={billboard} />
+                <BillboardForm initialData={product} />
             </div>
         </div>
         
     );
 }
  
-export default BillboardPage;
+export default ProductPage;
