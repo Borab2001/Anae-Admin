a3adbed0ae6cea8253ebe759c7ada24bc6fb59ad
Bora <borabalos9@gmail.com>
2023-12-06 15:08:36 +1100

    Product get route

--- anae-admin/app/api/[storeId]/products/route.ts
+++ anae-admin/app/api/[storeId]/products/route.ts
@@ -98,20 +98,41 @@ export async function GET(
     { params }: { params: { storeId: string } }
 ) {
     try {
+        const { searchParams } = new URL(req.url);
+        const categoryId = searchParams.get("categoryId") || undefined;
+        const colorId = searchParams.get("colorId") || undefined;
+        const sizeId = searchParams.get("sizeId") || undefined;
+        const isFeatured = searchParams.get("isFeatured");
+
+
         if (!params.storeId) {
             return new NextResponse("Store ID is required", { status: 400 });
         }
 
-        const billboards = await prismadb.billboard.findMany({
+        const products = await prismadb.product.findMany({
             where: {
                 storeId: params.storeId,
+                categoryId,
+                colorId,
+                sizeId,
+                isFeatured: isFeatured ? true : undefined,
+                isArchived: false
+            },
+            include: {
+                images: true,
+                category: true,
+                color: true,
+                size: true
+            },
+            orderBy: {
+                createdAt: 'desc'
             }
         });
 
-        return NextResponse.json(billboards);
+        return NextResponse.json(products);
 
     } catch (error) {
-        console.log('[BILLBOARDS_GET]', error);
+        console.log('[PRODUCTS_GET]', error);
         return new NextResponse("Internal error", { status: 500 });
     }
 }
