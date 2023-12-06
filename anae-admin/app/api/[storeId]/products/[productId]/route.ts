2fb9df226af55ad039ab16caf2ac0d930f40999d
Bora <borabalos9@gmail.com>
2023-12-06 15:46:14 +1100

    [productId] patch route

--- anae-admin/app/api/[storeId]/products/[productId]/route.ts
+++ anae-admin/app/api/[storeId]/products/[productId]/route.ts
@@ -33,28 +33,53 @@ export async function GET (
 
 export async function PATCH (
     req: Request,
-    { params }: { params: { storeId: string, billboardId: string } }
+    { params }: { params: { storeId: string, productId: string } }
 ) {
     try {
         const { userId } = auth();
         const body = await req.json();
 
-        const { label, imageUrl } = body;
+        const {
+            name,
+            price,
+            categoryId,
+            colorId,
+            sizeId,
+            images,
+            isFeatured,
+            isArchived
+        } = body;
 
         if (!userId) {
             return new NextResponse("Unauthenticated", { status: 401 });
         }
 
-        if (!label) {
-            return new NextResponse("Label is required", { status: 400 });
+        if (!name) {
+            return new NextResponse("Name is required", { status: 400 });
         }
 
-        if (!imageUrl) {
-            return new NextResponse("Image URL is required", { status: 400 });
+        if (!images || !images.length) {
+            return new NextResponse("Images are required", { status: 400 });
         }
 
-        if (!params.billboardId) {
-            return new NextResponse("Billboard ID is required", { status: 400 });
+        if (!price) {
+            return new NextResponse("Price is required", { status: 400 });
+        }
+
+        if (!categoryId) {
+            return new NextResponse("Category ID is required", { status: 400 });
+        }
+
+        if (!colorId) {
+            return new NextResponse("Color ID is required", { status: 400 });
+        }
+
+        if (!sizeId) {
+            return new NextResponse("Size ID is required", { status: 400 });
+        }
+
+        if (!params.productId) {
+            return new NextResponse("Product ID is required", { status: 400 });
         }
 
         const storeByUserId = await prismadb.store.findFirst({
@@ -68,19 +93,42 @@ export async function PATCH (
             return new NextResponse("Unauthorized", { status: 403 });
         }
 
-        const billboard = await prismadb.billboard.updateMany({
+        await prismadb.product.update({
             where: {
-                id: params.billboardId,
+                id: params.productId,
             },
             data: {
-                label,
-                imageUrl
+                name,
+                price,
+                categoryId,
+                colorId,
+                sizeId,
+                images: {
+                    deleteMany: {},
+                },
+                isFeatured,
+                isArchived,
             }
         });
 
-        return NextResponse.json(billboard);
+        const product = await prismadb.product.update({
+            where: {
+                id: params.productId
+            },
+            data: {
+                images: {
+                    createMany: {
+                        data: [
+                            ...images.map((image : { url: string }) => image)
+                        ]
+                    }
+                }
+            }
+        })
+
+        return NextResponse.json(product);
     } catch (error) {
-        console.log('[BILLBOARD_PATCH]', error);
+        console.log('[PRODUCT_PATCH]', error);
         return new NextResponse("Internal error", { status: 500 });
     }
 }
