89e44e9feb5d1a5088de623b6c1785b72222dd15
Bora <borabalos9@gmail.com>
2024-01-26 13:19:50 +0100

    Checkout route POST function

--- anae-admin/app/api/[storeId]/checkout/route.ts
+++ anae-admin/app/api/[storeId]/checkout/route.ts
@@ -13,3 +13,53 @@ const corsHeaders = {
 export async function OPTIONS() {
     return NextResponse.json({}, { headers: corsHeaders });
 }
+
+export async function POST(
+    req: Request,
+    { params }: { params: { storeId: string } }
+) {
+    const { productIds } = await req.json();
+
+    if (!productIds || productIds.length === 0) {
+        return new NextResponse("Product IDs are required", { status: 400 });
+    }
+
+    const products = await prismadb.product.findMany({
+        where: {
+            id: {
+                in: productIds
+            }
+        }
+    });
+
+    const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = [];
+
+    products.forEach((product) => {
+        line_items.push({
+            quantity: 1,
+            price_data: {
+                currency: 'EUR',
+                product_data: {
+                    name: product.name,
+                },
+                unit_amount: product.price.toNumber() * 100,
+            }
+        });
+    });
+
+    const order = await prismadb.order.create({
+        data: {
+            storeId: params.storeId,
+            isPaid: false,
+            orderItems: {
+                create: productIds.map((productId: string) => {
+                    product: {
+                        connect: {
+                            id: productId
+                        }
+                    }
+                })
+            }
+        }
+    })
+}
