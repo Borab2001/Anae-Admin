aa4dcb6caf8611dced5330952cf0b5343e6dc32e
Bora <borabalos9@gmail.com>
2024-01-26 17:27:02 +0100

    Revert and cleaning

--- anae-admin/app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
+++ anae-admin/app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
@@ -4,7 +4,7 @@ import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 import { DataTable } from "@/components/ui/data-table";
 
-import { OrderColumn, columns } from "./columns";
+import { columns, OrderColumn } from "./columns";
 
 interface OrderClientProps {
     data: OrderColumn[]
