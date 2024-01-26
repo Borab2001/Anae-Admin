33e0a61f64d33e509d82a9309616b2958902d25b
Bora <borabalos9@gmail.com>
2024-01-26 10:47:09 +0100

    Checkout route with Options function

---
+++ anae-admin/app/api/[storeId]/checkout/route.ts
@@ -0,0 +1,15 @@
+import Stripe from "stripe";
+import { NextResponse } from "next/server";
+
+import { stripe } from "@/lib/stripe";
+import prismadb from "@/lib/prismadb";
+
+const corsHeaders = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization",
+}
+
+export async function OPTIONS() {
+    return NextResponse.json({}, { headers: corsHeaders });
+}
