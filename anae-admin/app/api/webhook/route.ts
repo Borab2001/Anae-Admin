f3c9089f402c2190e50b0ce462b2dfd58946fb33
Bora <borabalos9@gmail.com>
2024-01-28 12:51:32 +0100

    Stripe webhook

---
+++ anae-admin/app/api/webhook/route.ts
@@ -0,0 +1,24 @@
+import Stripe from "stripe";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
+
+import { stripe } from "@/lib/stripe";
+import prismadb from "@/lib/prismadb";
+
+export async function POST(req: Request) {
+    
+    const body = await req.text();
+    const signature = headers().get("Stripe-Signature") as string;
+
+    let event: Stripe.Event;
+
+    try {
+        event = stripe.webhooks.constructEvent(
+            body,
+            signature,
+            process.env.STRIPE_WEBHOOK_SECRET!
+        )
+    } catch (error: any) {
+        return new NextResponse(`Webhook Error: ${error.message}`, { status: 400 });
+    }
+}
