1a288416ea0e7b9784c9f501d2de8035e02b8dbd
Bora <borabalos9@gmail.com>
2024-02-19 20:22:50 +0100

    Card header for dashboard w/ shadcn card component

--- anae-admin/app/(dashboard)/[storeId]/(routes)/page.tsx
+++ anae-admin/app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,6 +1,8 @@
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 import prismadb from "@/lib/prismadb";
+import { DollarSign } from "lucide-react";
 
 interface DashboardPageProps {
     params: { 
@@ -18,7 +20,14 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({
                 <Heading title="Dashboard" description="Overview of your store" />
                 <Separator />
                 <div className="grid gap-4 grid-cols-3">
-
+                    <Card>
+                        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                            <CardTitle className="text-sm font-medium">
+                                Total Revenue
+                            </CardTitle>
+                            <DollarSign className="h-4 w-4 text-muted-foreground" />
+                        </CardHeader>
+                    </Card>
                 </div>
             </div>
         </div>
