dd66796836f746c1a2e2e70f6ec095c3629b4381
Bora <borabalos9@gmail.com>
2024-02-19 20:14:32 +0100

    Restyling the dashboard

--- anae-admin/app/(dashboard)/[storeId]/(routes)/page.tsx
+++ anae-admin/app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,3 +1,5 @@
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
 import prismadb from "@/lib/prismadb";
 
 interface DashboardPageProps {
@@ -9,15 +11,16 @@ interface DashboardPageProps {
 const DashboardPage: React.FC<DashboardPageProps> = async ({
     params
 }) => {
-    const store = await prismadb.store.findFirst({
-        where: {
-            id: params.storeId
-        }
-    });
 
     return (
-        <div>
-            Active Store: {store?.name}
+        <div className="flex-col">
+            <div className="flex-1 space-y-4 p-8 pt-6">
+                <Heading title="Dashboard" description="Overview of your store" />
+                <Separator />
+                <div className="grid gap-4 grid-cols-3">
+
+                </div>
+            </div>
         </div>
     );
 }
