daa0e9ebb0c58f0b1328a73708b52293ad374ab7
Bora <borabalos9@gmail.com>
2024-02-19 20:35:28 +0100

    Card content for total revenue

--- anae-admin/app/(dashboard)/[storeId]/(routes)/page.tsx
+++ anae-admin/app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,7 +1,8 @@
-import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 import prismadb from "@/lib/prismadb";
+import { formatter } from "@/lib/utils";
 import { DollarSign } from "lucide-react";
 
 interface DashboardPageProps {
@@ -27,6 +28,24 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({
                             </CardTitle>
                             <DollarSign className="h-4 w-4 text-muted-foreground" />
                         </CardHeader>
+                        <CardContent>
+                            <div className="text-2xl font-bold">
+                                {formatter.format(100)}
+                            </div>
+                        </CardContent>
+                    </Card>
+                    <Card>
+                        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                            <CardTitle className="text-sm font-medium">
+                                Total Revenue
+                            </CardTitle>
+                            <DollarSign className="h-4 w-4 text-muted-foreground" />
+                        </CardHeader>
+                        <CardContent>
+                            <div className="text-2xl font-bold">
+                                {formatter.format(100)}
+                            </div>
+                        </CardContent>
                     </Card>
                 </div>
             </div>
