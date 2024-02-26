85e86644f3c859d8b4629d226b660f21c58e5a30
Bora <borabalos9@gmail.com>
2024-02-26 11:32:30 +0100

    Graph data with each month

--- anae-admin/actions/get-graph-revenue.ts
+++ anae-admin/actions/get-graph-revenue.ts
@@ -1,5 +1,10 @@
 import prismadb from "@/lib/prismadb";
 
+interface GraphData {
+    name: string;
+    total: number;
+}
+
 export const getGraphRevenue = async (storeId: string) => {
     const paidOrders = await prismadb.order.findMany({
         where: {
@@ -27,4 +32,25 @@ export const getGraphRevenue = async (storeId: string) => {
 
         monthlyRevenue[month] = (monthlyRevenue[month] || 0) + revenueForOrder;
     };
+
+    const graphData: GraphData[] = [
+        { name:"Jan", total: 0 },
+        { name:"Feb", total: 0 },
+        { name:"Mar", total: 0 },
+        { name:"Apr", total: 0 },
+        { name:"May", total: 0 },
+        { name:"Jun", total: 0 },
+        { name:"Jul", total: 0 },
+        { name:"Aug", total: 0 },
+        { name:"Sep", total: 0 },
+        { name:"Oct", total: 0 },
+        { name:"Nov", total: 0 },
+        { name:"Dec", total: 0 },
+    ];
+
+    for (const month in monthlyRevenue) {
+        graphData[parseInt(month)].total = monthlyRevenue[parseInt(month)];
+    }
+
+    return graphData;
 }
--- anae-admin/app/(dashboard)/[storeId]/(routes)/page.tsx
+++ anae-admin/app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -8,6 +8,7 @@ import { formatter } from "@/lib/utils";
 import { getTotalRevenue } from "@/actions/get-total-revenue";
 import { getSalesCount } from "@/actions/get-sales-count";
 import { getStockCount } from "@/actions/get-stock-count";
+import { getGraphRevenue } from "@/actions/get-graph-revenue";
 import { Overview } from "@/components/overview";
 
 interface DashboardPageProps {
@@ -23,6 +24,7 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({
     const totalRevenue = await getTotalRevenue(params.storeId);
     const salesCount = await getSalesCount(params.storeId);
     const stockCount = await getStockCount(params.storeId);
+    const graphRevenue = await getGraphRevenue(params.storeId);
 
     return (
         <div className="flex-col">
@@ -78,7 +80,7 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({
                         <CardTitle>Overview</CardTitle>
                     </CardHeader>
                     <CardContent className="pl-2">
-                        <Overview data={[]} />
+                        <Overview data={graphRevenue} />
                     </CardContent>
                 </Card>
             </div>
